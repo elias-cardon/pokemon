1f581b07138503057671a074f29580b0cc23ab23
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-03-31 10:20:02 +0200

    ajout de la méthode get_defense

--- types_pokemon.py
+++ types_pokemon.py
@@ -18,6 +18,9 @@ class Eau(Pokemon):
         defense = self.calculer_statistique(niveau, base_defense, iv_defense, ev_defense, nature_defense)
         super().__init__(nom, niveau, attaque, defense, "Eau")
 
+    def get_defense(self):
+        return self.defense
+
 class Terre(Pokemon):
     def __init__(self, nom, niveau, base_attaque, base_defense, iv_attaque=0, iv_defense=0, ev_attaque=0, ev_defense=0, nature_attaque=1, nature_defense=1):
         attaque = self.calculer_statistique(niveau, base_attaque, iv_attaque, ev_attaque, nature_attaque)
